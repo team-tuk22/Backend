3f2662a5ab8737e4ffec9e2de5a3b1179a0af3cb
최민호 <Minho@choeminhoui-Macmini.local>
2025-09-23 22:09:36 +0900

    크롤링

--- app/models/model.py
+++ app/models/model.py
@@ -8,22 +8,22 @@ from app.databases.database import Base
 class Judgement(Base):
     __tablename__ = "judgements"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4) # 일련번호
 
-    case_name = Column(String, index=True, nullable=False)
-    case_number = Column(String, index=True, nullable=False)
-    case_date = Column(DateTime, index=True, nullable=False)
-    case_result = Column(String, index=True, nullable=False)
-    case_court = Column(String, index=True, nullable=False)
-    case_court_code = Column(Integer, index=True, nullable=False)
-    case_type = Column(String, index=True, nullable=False)
-    case_type_code = Column(Integer, index=True, nullable=False)
-    case_result_type = Column(String, index=True, nullable=False)
-    case_result_decision = Column(String, index=True, nullable=False)
-    case_result_summary = Column(Text, nullable=True)
-    reference = Column(String, nullable=True)
-    reference_case = Column(String, nullable=True)
-    case_precedent = Column(Text, nullable=True)
+    case_name = Column(String, index=True, nullable=False)                # 사건명
+    case_number = Column(String, index=True, nullable=False)              # 사건번호
+    case_date = Column(DateTime, index=True, nullable=False)              # 선고일자
+    case_result = Column(String, index=True, nullable=False)              # 선고
+    case_court = Column(String, index=True)                               # 법원명      
+    case_court_code = Column(Integer, index=True)                         # 법원코드
+    case_type = Column(String, index=True)                                # 사건종류
+    case_type_code = Column(Integer, index=True)                          # 사건종류코드
+    case_result_type = Column(String, index=True)                         # 판결유형
+    case_result_decision = Column(String, index=True, nullable=False)     # 판결시사항
+    case_result_summary = Column(Text, nullable=True)                     # 판결요지
+    reference = Column(String, nullable=True)                             # 참조조문
+    reference_case = Column(String, nullable=True)                        # 참조판례
+    case_precedent = Column(Text, nullable=True)                          # 판례요지
 
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
-    updated_at = Column(DateTime, default=datetime.now)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)   # 생성일자
+    updated_at = Column(DateTime, default=datetime.now)                   # 수정일자 
